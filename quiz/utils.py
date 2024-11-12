fc7385ed85ce3656dd56764f54eaf4b57f7dea24
Bartosz Dębek <68703788+bartdebek@users.noreply.github.com>
2024-11-12 23:45:46 +0100

    test

--- quiz/utils.py
+++ quiz/utils.py
@@ -4,10 +4,34 @@ import base64
 
 from environs import Env
 
-from consts import TOP_100_ARTISTS
+# from quiz.utils import TOP_100_ARTISTS
 
 env = Env()
 env.read_env()
+
+TOP_100_ARTISTS = [
+    "The Beatles", "Elvis Presley", "Michael Jackson", "Madonna", "The Rolling Stones",
+    "Bob Dylan", "Led Zeppelin", "Prince", "Pink Floyd", "Aretha Franklin",
+    "Stevie Wonder", "David Bowie", "Queen", "Marvin Gaye", "The Beach Boys",
+    "Jimi Hendrix", "The Who", "Ray Charles", "Johnny Cash", "Elton John",
+    "Nirvana", "Bruce Springsteen", "The Supremes", "Kanye West", "The Eagles",
+    "Jay-Z", "Metallica", "Beyoncé", "The Doors", "Bob Marley",
+    "U2", "Outkast", "R.E.M.", "Eminem", "Adele",
+    "Patsy Cline", "Radiohead", "The Clash", "The Police", "Janet Jackson",
+    "Otis Redding", "Whitney Houston", "Tupac Shakur", "James Brown", "Kendrick Lamar",
+    "Fleetwood Mac", "Smokey Robinson", "The Temptations", "Paul Simon", "The Kinks",
+    "Simon & Garfunkel", "Creedence Clearwater Revival", "Public Enemy", "The Velvet Underground", "Santana",
+    "Billy Joel", "Green Day", "Eric Clapton", "LL Cool J", "The Allman Brothers Band",
+    "George Michael", "Little Richard", "Aerosmith", "Rihanna", "Missy Elliott",
+    "Neil Young", "Chuck Berry", "Van Halen", "The Jackson 5", "Joni Mitchell",
+    "Sly & The Family Stone", "Frank Sinatra", "Guns N' Roses", "The Ramones", "Al Green",
+    "Sam Cooke", "Louis Armstrong", "Dr. Dre", "Beastie Boys", "Curtis Mayfield",
+    "Notorious B.I.G.", "Nine Inch Nails", "Johnny Cash", "Pearl Jam", "Tina Turner",
+    "Diana Ross", "Dusty Springfield", "John Lennon", "Elvis Costello", "Bon Jovi",
+    "Red Hot Chili Peppers", "Bee Gees", "Etta James", "The Byrds", "Patti Smith",
+    "Bo Diddley", "Genesis", "Carole King", "Foo Fighters", "James Taylor",
+    "Dolly Parton", "The Cure", "KISS", "B.B. King", "The Zombies"
+]
 
 
 class SpotifyGenerator:
